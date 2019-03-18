d0e5da3ac1cce4910f3abd4a3bb518a752a48956
Takeshi Nakamura <taqueci.n@gmail.com>
2019-03-18 22:42:48 +0900

    Fix NoMethodError of image_tag

--- lib/application_helper_avatar_patch.rb
+++ lib/application_helper_avatar_patch.rb
@@ -38,7 +38,7 @@ module LocalAvatarsPlugin
 				av = user.attachments.find_by_description 'avatar'
 				if av then
 					image_url = url_for :only_path => true, :controller => 'account', :action => 'get_avatar', :id => user
-					return image_tag(image_url, options.reverse_merge(:class => 'gravatar'))
+					return ActionController::Base.helpers.image_tag(image_url, options.reverse_merge(:class => 'gravatar'))
 				end
 			end
 
@@ -47,7 +47,7 @@ module LocalAvatarsPlugin
 			if avtr.present?
 				return avtr
 			else
-				return image_tag('default.png', options.reverse_merge(:plugin => 'redmine_local_avatars', :class => 'gravatar'))
+				return ActionController::Base.helpers.image_tag('/plugin_assets/redmine_local_avatars/images/default.png', options.reverse_merge(:class => 'gravatar'))
 			end
 		end
   end
